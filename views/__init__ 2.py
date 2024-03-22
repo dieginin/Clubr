9a4d82e314b559b373675c5bd91b29a30574751b
dieginin <diegobalestra9@gmail.com>
2024-03-22 18:08:34 -0400

    chore: delete '__init__ 2.py'

--- views/__init__ 2.py
+++
@@ -1,6 +0,0 @@
-from .brawlers_view import BrawlersView
-from .home_view import HomeView
-from .members_view import MembersView
-from .no_club_view import NoClubView
-from .past_members_view import PastMembersView
-from .valorate_view import ValorateView
