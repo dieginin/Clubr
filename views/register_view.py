25de6f23d09750ac5abbc8283f4e9900e3f3561f
dieginin <diegobalestra9@gmail.com>
2024-03-22 18:49:20 -0400

    refactor

--- views/register_view.py
+++ views/register_view.py
@@ -29,33 +29,40 @@ class RegisterView(View):
         self.fb = fb
         self.pg = page
 
-        self.floating_action_button = FloatingActionButton(
-            "Regresar", on_click=lambda _: self.pg.go("/login"), width=85
-        )
+        self.init_ui()
 
+    def init_ui(self):
+        self.setup_layout()
+        self.setup_fields()
+        self.setup_controls()
+
+    def setup_layout(self):
         self.route = "/register"
         self.bgcolor = "primary"
         self.vertical_alignment = MainAxisAlignment.CENTER
         self.horizontal_alignment = CrossAxisAlignment.CENTER
 
-        self.code_field = CodeField("Código", self.set_code)
-
-        self.club_field = CodeField("Club Tag", self.register)
-        self.mail_field = MailField("Correo", self.register)
-        self.pass_field = PasswordField("Contraseña", self.register)
+    def setup_fields(self):
+        self.code_field = CodeField("Código", self.handle_code_input)
+        self.club_field = CodeField("Club Tag", self.handle_register)
+        self.mail_field = MailField("Correo", self.handle_register)
+        self.pass_field = PasswordField("Contraseña", self.handle_register)
         self.pcon_field = RepeatPasswordField(
-            self.pass_field, "Confirmar Contraseña", self.register
+            self.pass_field, "Confirmar Contraseña", self.handle_register
         )
-        self.user_field = UserField("Usuario", self.register)
+        self.user_field = UserField("Usuario", self.handle_register)
 
+    def setup_controls(self):
         self.code_sectn = CenterColumn(
             [
                 Title("Ingresa el código de tu club"),
                 self.code_field,
-                ElevatedButton("Guardar", on_click=self.set_code),
+                ElevatedButton("Guardar", on_click=self.handle_code_input),
                 SecondaryButton("No tengo código", on_click=self.no_code),
-            ]
+            ],
+            visible=True,
         )
+
         self.info_sectn = CenterColumn(
             [
                 Title("Ingresa tus datos"),
@@ -68,108 +75,64 @@ class RegisterView(View):
                         self.club_field,
                     ]
                 ),
-                ElevatedButton("Registrarse", on_click=self.register),
+                ElevatedButton("Registrarse", on_click=self.handle_register),
             ],
             visible=False,
             width=340,
         )
 
-        self.controls = [
-            AuthCard(
-                [self.code_sectn, self.info_sectn],
-                style="slim",
-            ),
-        ]
+        self.controls = [AuthCard([self.code_sectn, self.info_sectn], style="slim")]
 
-    def set_code(self, _) -> None:
-        if self.code_field.value == "" or self.code_field.color == "red":
+        self.floating_action_button = FloatingActionButton(
+            "Regresar", on_click=lambda _: self.pg.go("/login"), width=85
+        )
+
+    def handle_code_input(self, _):
+        code = self.code_field.value
+        if code == "" or self.invalid_code(code):
             error_snackbar(self.pg, "Ingrese un código válido")
             self.code_field.focus()
             return
+        self.switch_views()
 
-        clubes = self.fb.db.child("clubs").get()
-        try:
-            if self.code_field.value not in set([u.val()["code"] for u in clubes]):
-                error_snackbar(self.pg, "El código no existe")
-                self.code_field.focus()
-                return
-        except:
-            error_snackbar(self.pg, "El código no existe")
-            self.code_field.focus()
-            return
+    def switch_views(self, option="code"):
+        if option == "code":
+            self.pg.splash = ProgressBar()
+            self.disabled = True
+            self.pg.update()
 
-        self.pg.splash = ProgressBar()
-        self.disabled = True
-        self.pg.update()
+            self.code_sectn.visible = False
+            self.info_sectn.visible = True
+            self.club_field.opacity = 0
+            self.update()
 
-        self.code_sectn.visible = False
-        self.info_sectn.visible = True
-        self.club_field.opacity = 0
-        self.update()
-
-        self.pg.splash = None
-        self.disabled = False
+            self.pg.splash = None
+            self.disabled = False
+        else:
+            self.code_sectn.visible = False
+            self.info_sectn.visible = True
         self.pg.update()
 
     def no_code(self, _):
-        self.code_sectn.visible = False
-        self.info_sectn.visible = True
-        self.update()
+        self.switch_views("nocode")
 
-    def register(self, _):
-        if self.mail_field.value == "":
-            error_snackbar(self.pg, "Ingrese un correo")
-            self.mail_field.focus()
-            return
-        if self.mail_field.color == "red":
-            error_snackbar(self.pg, "Ingrese un correo válido")
-            self.mail_field.focus()
-            return
-        if self.user_field.value == "":
-            error_snackbar(self.pg, "Ingrese un usuario")
-            self.user_field.focus()
-            return
-        if self.user_field.color == "red":
-            error_snackbar(self.pg, "Ingrese un usuario válido")
-            self.user_field.focus()
-            return
-        if self.pass_field.value == "":
-            error_snackbar(self.pg, "Ingrese una contraseña")
-            self.pass_field.focus()
-            return
-        if self.pass_field.color == "red":
-            error_snackbar(self.pg, "Ingrese una contraseña válida")
-            self.pass_field.focus()
-            return
-        if self.pcon_field.value == "" or self.pcon_field.color == "red":
-            error_snackbar(self.pg, "Ingrese contraseñas que coincida")
-            self.pcon_field.focus()
-            return
-        if self.club_field.opacity == 1 and (
-            self.club_field.value == "" or self.club_field.color == "red"
-        ):
-            error_snackbar(self.pg, "Ingrese un tag de club válido")
-            self.club_field.focus()
+    def invalid_code(self, code):
+        try:
+            return code not in {
+                u.val()["code"] for u in self.fb.db.child("clubs").get()
+            }
+        except:
+            return True
+
+    def handle_register(self, _):
+        if not self.validate_fields():
             return
 
         self.pg.splash = ProgressBar()
         self.disabled = True
         self.pg.update()
 
-        if self.club_field.opacity == 1:
-            res = self.fb.register_new_user(
-                self.club_field.value,
-                self.user_field.value,
-                self.mail_field.value,
-                self.pass_field.value,
-            )
-        else:
-            res = self.fb.register_code_user(
-                self.code_field.value,
-                self.user_field.value,
-                self.mail_field.value,
-                self.pass_field.value,
-            )
+        res = self.register_user()
 
         self.pg.splash = None
         self.disabled = False
@@ -178,3 +141,38 @@ class RegisterView(View):
             self.pg.go("/")
         else:
             error_snackbar(self.pg, res)
+
+    def validate_fields(self):
+        fields = [
+            (self.mail_field, "Ingrese un correo válido"),
+            (self.user_field, "Ingrese un usuario válido"),
+            (self.pass_field, "Ingrese una contraseña válida"),
+            (self.pcon_field, "Ingrese contraseñas que coincidan"),
+        ]
+
+        if self.club_field.opacity == 1:
+            fields.append((self.club_field, "Ingrese un tag de club válido"))
+
+        for field, error_message in fields:
+            if field.value == "" or field.color == "red":
+                error_snackbar(self.pg, error_message)
+                field.focus()
+                return False
+
+        return True
+
+    def register_user(self):
+        if self.club_field.opacity == 1:
+            return self.fb.register_new_user(
+                self.club_field.value,
+                self.user_field.value,
+                self.mail_field.value,
+                self.pass_field.value,
+            )
+        else:
+            return self.fb.register_code_user(
+                self.code_field.value,
+                self.user_field.value,
+                self.mail_field.value,
+                self.pass_field.value,
+            )
