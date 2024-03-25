c0e8ea915190f172781118b2905be2e2005d6848
dieginin <diegobalestra9@gmail.com>
2024-03-25 17:00:37 -0400

    update utils/__init__.py and helpers.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,10 @@
 from .config import FIREBASE_CONFIG, ROYALE_TOKEN, URL_ROYALE
-from .helpers import error_snackbar, success_snackbar
+from .helpers import (
+    error_snackbar,
+    from_bool,
+    from_int,
+    from_list,
+    from_str,
+    success_snackbar,
+)
 from .validator import Validator
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,28 @@
+from typing import Any, Callable, List, TypeVar
+
 from flet import Page, SnackBar, Text
+
+T = TypeVar("T")
+
+
+def from_bool(x: Any) -> bool:
+    assert isinstance(x, bool)
+    return x
+
+
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+
+def from_str(x: Any) -> str:
+    assert isinstance(x, str)
+    return x
+
+
+def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
+    assert isinstance(x, list)
+    return [f(y) for y in x]
 
 
 def error_snackbar(page: Page, message: str | None = None):
